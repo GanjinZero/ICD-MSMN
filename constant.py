0b27625641a81054dd9ad7e89c10362ed4ce8eb9
GanjinZero <yuanz17@mails.tsinghua.edu.cn>
2022-05-06 14:15:53 +0800

    remove hard code

--- constant.py
+++ constant.py
@@ -3,6 +3,5 @@ import os
 
 # change DATA_DIR for yourself
 # DATA_DIR = "./sample_data/"
-DATA_DIR = "/nas-alinlp/zheng/caml-mimic/mimicdata/"
 MIMIC_2_DIR = os.path.join(DATA_DIR, "mimic2")
 MIMIC_3_DIR = os.path.join(DATA_DIR, "mimic3")
